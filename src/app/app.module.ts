786174f1152ee80e88841fd5e534357623d706a0
Vladimir <mendozahum20@gmail.com>
2021-06-12 15:05:13 -0300

    Modulos, Rutas hijas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,31 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 // Routes
 import { AppRoutingModule } from './app-rounting.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { PagesComponent } from './pages/pages.component';
+
+// Modulos
+import { PagesModule } from './pages/pages.module';
+import { AuthModule } from './auth/auth.module';
+
+// Components
+import { NopagefoundComponent } from './400/nopagefound.component';
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    DashboardComponent,
     NopagefoundComponent,
-    ProgressComponent,
-    Grafica1Component,
-    PagesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
